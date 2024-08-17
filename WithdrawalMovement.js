8556f2777660d199ca07cd06ccacd195daed14f7
abdallahkrj <abdallahkrj@gmail.com>
2024-08-18 01:15:35 +0300

    camera and move fix

--- WithdrawalMovement.js
+++ WithdrawalMovement.js
@@ -7,7 +7,7 @@ class WithdrawalMovement {
         this.waterDensity = 1025; // density of water in kg/m^3
         this.gravityAcc = 9.81; // acceleration due to gravity in m/s^2
         this.fraction_constant = 0.02;
-        this.projectedAreas = [2.1, 2.1,3.6]; // Areas should not be empty, assume some default values
+        this.projectedAreas = [2.1, 2.1, 3.6]; // Areas should not be empty, assume some default values
         this.deltaTime = 0.05;
         this.velocity = new Vector3(0, 0, 0);
         this.position = new Vector3(0, 0, 0);
@@ -15,19 +15,19 @@ class WithdrawalMovement {
         this.maxDepth = 1000; // maximum depth limit
         this.volume = 1.56; // Volume of the submarine in cubic meters
         this.pressureThreshold = 1.1; // Threshold ratio for pressure vs gravity force
-        this.warningDepthThreshold = 110;
+        this.warningDepthThreshold = 210;
         this.errorState = false; // New state to handle error
     }
 
     // Weight force due to gravity
     calculateWeightForce(tanks_water_volume) {
         const tanks_water_mass = tanks_water_volume * this.waterDensity;
-        return new Vector3(0, -this.gravityAcc * (this.empty_mass + tanks_water_mass) , 0);
+        return new Vector3(0, -this.gravityAcc * (this.empty_mass + tanks_water_mass), 0);
     }
 
     // Engine thrust force
-    engineThrust(fan_speed) {
-        return new Vector3(0, 0, this.power * fan_speed);
+    engineThrust(fan_speed, rotation) {
+        return new Vector3(0, 0, this.power * fan_speed).applyEuler(rotation);
     }
 
     // Archimedes' force (buoyant force)
@@ -37,30 +37,41 @@ class WithdrawalMovement {
 
     // Friction force
     calculateFrictionForce() {
-        const dragForceX =- 0.5 * this.fraction_constant * this.waterDensity * this.projectedAreas[0] * this.velocity.x;
+        const dragForceX = - 0.5 * this.fraction_constant * this.waterDensity * this.projectedAreas[0] * this.velocity.x;
         const dragForceY = -0.5 * this.fraction_constant * this.waterDensity * this.projectedAreas[1] * this.velocity.y;
-        const dragForceZ =- 0.5 * this.fraction_constant * this.waterDensity * this.projectedAreas[2] * this.velocity.z;
+        const dragForceZ = - 0.5 * this.fraction_constant * this.waterDensity * this.projectedAreas[2] * this.velocity.z;
 
         return new Vector3(dragForceX, dragForceY, dragForceZ);
     }
 
     // Pressure force due to depth
     calculatePressureForce() {
-        const depth =  this.position.y; // Depth is the negative y position
-        return new Vector3(0, this.waterDensity * this.gravityAcc * depth , 0);
+        const depth = this.position.y; // Depth is the negative y position
+        return new Vector3(0, this.waterDensity * this.gravityAcc * depth, 0);
     }
 
     // Check if pressure exceeds threshold and display a warning message
     checkPressureWarning() {
         const depth = -this.position.y;
-        if (depth > this.warningDepthThreshold && !this.errorState) {
+        if (depth > this.warningDepthThreshold && depth < this.warningDepthThreshold+1 && !this.errorState) {
             this.errorState = true; // Activate error state
             this.velocity.set(0, 0, 0); // Stop the submarine
             this.acceleration.set(0, 0, 0); // Stop acceleration
 
+        
             console.warn('Warning: Descending further may expose the submarine to excessive pressure!');
             alert('Warning: Descending further may expose the submarine to excessive pressure! Please click OK to resume.');
         }
+        if (depth < 0 && !this.errorState) {
+            // this.errorState = true; // Activate error state
+            this.position.setY(0); // Stop the submarine
+            this.velocity.set(0, 0, 0); // Stop the submarine
+            this.acceleration.set(0, 0, 0); // Stop acceleration
+
+            // console.warn('Warning: Descending further may expose the submarine to excessive pressure!');
+            // alert('Warning: Descending further may expose the submarine to excessive pressure! Please click OK to resume.');
+        }
+        // if(depth > this.)
     }
 
     // Method to resume movement after error state
@@ -73,21 +84,21 @@ class WithdrawalMovement {
 
 
     // Total forces affecting the submarine
-    calculateForce(tanks_water_volume, fan_speed, submarinDensity) {
-        const engineThrust = this.engineThrust(fan_speed).clone();
+    calculateForce(tanks_water_volume, fan_speed, submarinDensity, rotation) {
+        const engineThrust = this.engineThrust(fan_speed, rotation).clone();
         const archimedesForce = this.calculateArchimedesForce().clone();
         const frictionForce = this.calculateFrictionForce().clone(); // Use the current velocity
-     //   const pressureForce = this.calculatePressureForce().clone();
+        //   const pressureForce = this.calculatePressureForce().clone();
 
         const weightForce = this.calculateWeightForce(tanks_water_volume).clone();
-      //  console.log(pressureForce);       
-         
+        //  console.log(pressureForce);       
+
         // return constantVector;
         return archimedesForce.add(weightForce).add(engineThrust).add(frictionForce);
     }
     // Current linear acceleration
-    currentLinearAccelerate(tanks_water_volume, fan_speed, submarinDensity) {
-        const force = this.calculateForce(tanks_water_volume, fan_speed, submarinDensity);
+    currentLinearAccelerate(tanks_water_volume, fan_speed, submarinDensity, rotation) {
+        const force = this.calculateForce(tanks_water_volume, fan_speed, submarinDensity, rotation);
         this.acceleration.copy(force.divideScalar(this.empty_mass + tanks_water_volume * this.waterDensity));
         // console.log(this.acceleration.y);
         return this.acceleration;
@@ -123,15 +134,15 @@ class WithdrawalMovement {
 
         let submarinDensity = 1025 + tanks_water_volume;
         // console.log(submarinDensity);
-        fan_speed = - fan_speed ;
+        fan_speed = - fan_speed;
 
-    
-            this.currentLinearAccelerate(tanks_water_volume, fan_speed);
-            this.nextVelocity();
-        
+
+        this.currentLinearAccelerate(tanks_water_volume, fan_speed, null, submarine.rotation);
+        this.nextVelocity();
+
 
         if (!this.shouldStopVerticalMovement()) {
-            this.currentLinearAccelerate(tanks_water_volume, fan_speed, submarinDensity);
+            this.currentLinearAccelerate(tanks_water_volume, fan_speed, submarinDensity, submarine.rotation);
             this.nextVelocity();
         } else {
             // If the forces are equal, stop vertical movement
@@ -144,13 +155,14 @@ class WithdrawalMovement {
         this.checkPressureWarning();
     }
 
-    
-    autoChangeDepth(SubmarineForcers, desired_depth){
-        var d = - this.position.y - desired_depth;
-        SubmarineForcers["tanks_water_volume"] = 20 - d + this.velocity.y*2;
-        if(   SubmarineForcers["tanks_water_volume"]<0)
-        {
-            SubmarineForcers["tanks_water_volume"] = 0 ;
+
+    autoChangeDepth(SubmarineForcers, desired_depth) {
+        if (desired_depth >= 0) {
+            var d = - this.position.y - desired_depth;
+            SubmarineForcers["tanks_water_volume"] = 20 - d + this.velocity.y * 2;
+            if (SubmarineForcers["tanks_water_volume"] < 0) {
+                SubmarineForcers["tanks_water_volume"] = 0;
+            }
         }
     }
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { GUI } from "dat.gui";
+
 import SubmarineRotation from "/SubmarineRotation";
 import WithdrawalMovement from "/WithdrawalMovement"
 // Create the scene
@@ -56,16 +57,25 @@ gltfLoader.load(
     }
 );
 
+const bottom = -350;
 
 // Load the underwater terrain model
 const terrainLoader = new GLTFLoader();
 terrainLoader.load(
     './models/underwater_terrain.glb', // Adjust the path to your terrain model
     (gltf) => {
-        const terrainModel = gltf.scene;
-        terrainModel.scale.set(100, 100, 100);
-        terrainModel.position.set(0, -260, 0);
-        scene.add(terrainModel);
+        var base = -10 * 200;
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                const terrainModel = gltf.scene.clone();
+                terrainModel.scale.set(60, 60, 60);
+                // Randomize the position within a larger cube
+                var x = base + i * 400;
+                var z = base + j * 400;
+                terrainModel.position.set(x, bottom, z);
+                scene.add(terrainModel);
+            }
+        }
     },
     (xhr) => {
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
@@ -84,11 +94,11 @@ seaweedLoader.load(
         // Create 200 instances of the seaweed model
         for (let i = 0; i < 200; i++) {
             const seaweedInstance = seaweedModel.clone();
-            seaweedInstance.scale.set(5, 5, 5);
+            seaweedInstance.scale.set(4, 4, 4);
             // Randomize the position within a larger cube
-            const cubeSize = 1500;
+            const cubeSize = 120 * 25;
             const x = Math.random() * cubeSize - cubeSize / 2;
-            const y = -250;
+            const y = bottom;
             const z = Math.random() * cubeSize - cubeSize / 2;
             seaweedInstance.position.set(x, y, z);
             scene.add(seaweedInstance);
@@ -121,48 +131,88 @@ const SubmarineForcers = {
 
 };
 const AutoMovementVars = {
-    desired_depth: 0
+    desired_depth: -1
 };
 
 
 var gui = new GUI();
 var rotationFolder = gui.addFolder('Submarine Rotation:');
 rotationFolder.add(SubmarineForcers, 'F_torque', -200, 200).name('Torque (τz)').step(10);
+rotationFolder.open();
 // rotationFolder.add(SubmarineForcers, 'tau_z', -1, 1).name('Torque (τz)').step(0.1);
 // rotationFolder.add(SubmarineForcers, 'I_z', 1, 10).name('Inertia (Iz)').step(0.1);
 // rotationFolder.add(SubmarineForcers, 'tau_y', -1, 1).name('Torque (τy)').step(0.1);
 // rotationFolder.add(SubmarineForcers, 'I_y', 1, 10).name('Inertia (Iy)').step(0.1);
 // rotationFolder.open();
 
-//const depthController = gui.add({ text: 'Depth: '+ withdrawalMovement.position.y }, 'text').name('Depth');rotationFolder.open();
 var WithdrawalFolder = gui.addFolder('Withdrawal movement :');
-WithdrawalFolder.add(SubmarineForcers, 'tanks_water_volume', 0, 100).name('V.tanks_water cm3').step(1);
+WithdrawalFolder.add(SubmarineForcers, 'tanks_water_volume', 0, 100).name('V.tanks_water cm3').step(1).onChange(() => AutoMovementVars["desired_depth"] = -1);
 WithdrawalFolder.add(SubmarineForcers, 'fan_speed', -10, 10).name('fan speed').step(0.1);
 WithdrawalFolder.open();
 
 var moveFolder = gui.addFolder('Auto Movement :');
-WithdrawalFolder.add(AutoMovementVars, 'desired_depth', 0, 100).name('Desired Depth').step(1);
+moveFolder.add(AutoMovementVars, 'desired_depth', 0, 100).name('Desired Depth').step(1);
+moveFolder.open();
+
+var infoGUI = new GUI();
+infoGUI.add(withdrawalMovement.position, "y").name('Depth');
+
+// var cameraAngle = 0;
+var angle = 1;
+
+
+document.addEventListener("keydown", (e) => onDocumentKey(e, true), false);
+document.addEventListener("keyup", (e) => onDocumentKey(e, false), false);
+function onDocumentKey(event, keydown) {
+    var keyCode = event.key;
+    if (keyCode == 'w') {
+        if (keydown) SubmarineForcers['fan_speed'] = 3;
+        else SubmarineForcers['fan_speed'] = 0
+    } else if (keyCode == 's') {
+        if (keydown) SubmarineForcers['fan_speed'] = -3;
+        else SubmarineForcers['fan_speed'] = 0
+    } if (keyCode == 'd') {
+        if (keydown) SubmarineForcers['F_torque'] = -100;
+        else SubmarineForcers['F_torque'] = 0
+    } else if (keyCode == 'a') {
+        if (keydown) SubmarineForcers['F_torque'] = 100;
+        else SubmarineForcers['F_torque'] = 0
+    } if (keyCode == 'Shift') {
+        if (keydown) {
+            AutoMovementVars['desired_depth'] = -1;
+            SubmarineForcers['tanks_water_volume'] = 40;
+        } else SubmarineForcers['tanks_water_volume'] = 20
+    } else if (keyCode == ' ') {
+        if (keydown) {
+            AutoMovementVars['desired_depth'] = -1;
+            SubmarineForcers['tanks_water_volume'] = 0;
+        } else SubmarineForcers['tanks_water_volume'] = 20
+    }
+    controls.update();
+};
 
 // Function to update the camera position based on the submarine's position
 function updateCamera() {
     if (!submarine) return;
-    // console.log(submarine.position);ب
-    camera.position.x = submarine.position.x;
-    camera.position.y = submarine.position.y;
-    camera.position.z = submarine.position.z + 10;
+    controls.target.x = submarine.position.x;
+    controls.target.y = submarine.position.y;
+    controls.target.z = submarine.position.z;
+    camera.position.setFromSphericalCoords(16, controls.getPolarAngle(), controls.getAzimuthalAngle());
+    camera.position.add(submarine.position);
     camera.lookAt(submarine.position);
 }
 // Animation loop
 function animate() {
     requestAnimationFrame(animate);
     if (submarine) {
-        submarineRotation.HorizontalAngularMotionInMoment(submarine, SubmarineForcers.F_torque,SubmarineForcers.tanks_water_volume*0.01);
+        submarineRotation.HorizontalAngularMotionInMoment(submarine, SubmarineForcers.F_torque, SubmarineForcers.tanks_water_volume * 0.01);
 
         // Update the submarine's position using the WithdrawalMovement position
-        withdrawalMovement.linearMotionInMoment(submarine, SubmarineForcers.tanks_water_volume*0.01, SubmarineForcers.fan_speed);
+        withdrawalMovement.linearMotionInMoment(submarine, SubmarineForcers.tanks_water_volume * 0.01, SubmarineForcers.fan_speed);
         withdrawalMovement.autoChangeDepth(SubmarineForcers, AutoMovementVars.desired_depth);
 
     }
+    infoGUI.updateDisplay();
     gui.updateDisplay();
     controls.update();
     updateCamera();
